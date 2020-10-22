4887b78a6b1af4b8512caf2be91377d90416e46b
Scheyk <kathrin.dostal@gmail.com>
2020-10-22 10:52:32 +0200

    methode getFullName finish

--- src/Kathi/Person.java
+++ src/Kathi/Person.java
@@ -2,13 +2,24 @@ package Kathi;
 
 public class Person {
 
-    String fName;
-    String lName;
-    int age;
+    private String fName;
+    private String lName;
+    private int age;
 
     public Person(String fName, String lName, int age){
         this.fName = fName;
         this.lName = lName;
         this.age = age;
+    }
+
+    public Person(){
+
+    }
+
+    static void getFullName(){
+        Person newPerson = new Person();
+        System.out.println("First Name: " + newPerson.fName);
+        System.out.println("Last Name: " + newPerson.lName);
+        System.out.println("Age: " + newPerson.age);
     }
 }
