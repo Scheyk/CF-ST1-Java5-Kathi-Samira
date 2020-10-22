9bf738aa40022f6a5f73f8dd91f94015f7aa1d6e
Scheyk <kathrin.dostal@gmail.com>
2020-10-22 13:20:47 +0200

    BankAccount finish

--- src/Kathi/BankAccount.java
+++ src/Kathi/BankAccount.java
@@ -2,41 +2,53 @@ package Kathi;
 
 public class BankAccount {
 
-    private int accountBalance;
-    private int addMoney;
-    private int withdrawMoney;
+    private String name;
+    private double accountBalance;
 
-    public BankAccount(int accountBalance, int addMoney, int withdrawMoney){
+    public BankAccount(String name, double accountBalance){
+        this.name = name;
         this.accountBalance = accountBalance;
-        this.addMoney = addMoney;
-        this.withdrawMoney = withdrawMoney;
     }
 
     public BankAccount(){
+    }
 
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(){
+        this.name = name;
     }
 
     public void setBalance(){
         this.accountBalance = accountBalance;
     }
 
-    public Integer getBalance(){
+    public Double getBalance(){
         return this.accountBalance;
     }
 
-    public void setAdd(){
-        this.addMoney = addMoney;
+    public void addMoney(double add){
+        System.out.println(this.name + "  " + this.accountBalance);
+       this.accountBalance += add;
+        System.out.println("Hello " + this.name + ", we added " + add + " on you Account.");
+        System.out.println("On your Account is now: " + this.accountBalance + " €");
+        System.out.println(" ");
     }
 
-    public Integer getAdd(){
-        return this.addMoney;
+    public void withdraw(double minus){
+        if(minus > this.accountBalance){
+            System.out.println(this.name + "  " + this.accountBalance);
+            System.out.println("Hello " + this.name + ", you have not enought money!");
+            System.out.println(" ");
+        } else {
+            System.out.println(this.name + "  " + this.accountBalance);
+            this.accountBalance -= minus;
+            System.out.println("Hello " + this.name + ", we have withdrawn " + minus + " from your Account.");
+            System.out.println("On your Account is now: " + this.accountBalance + " €");
+            System.out.println(" ");
+        }
     }
 
-    public void setWithdraw(){
-        this.withdrawMoney = withdrawMoney;
-    }
-
-    public Integer getWithdraw(){
-        return this.withdrawMoney;
-    }
 }
--- src/Kathi/Main.java
+++ src/Kathi/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[]args){
 
+        /*  Task 1
         Person one = new Person("Susi", "Maier", 25);
         Person two = new Person("Herbert", "Wald", 33);
 
@@ -12,6 +13,15 @@ public class Main {
         System.out.println("first Name: " + two.getfName());
         System.out.println("last name: " + two.getlName());
         System.out.println("last name: " + one.getlName().toUpperCase());
+         */
+
+        BankAccount one = new BankAccount("Susi",440);
+        BankAccount two = new BankAccount("Herbert",546);
+
+        one.addMoney(40);
+        one.withdraw(500);
+        two.withdraw(30);
+        two.addMoney(400);
 
 
     }
